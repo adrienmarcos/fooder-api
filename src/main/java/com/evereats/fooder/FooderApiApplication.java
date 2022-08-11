3027a446f6f9a3ea2f27336aa964a355c4486d4e
Adrien Rossato <adrien.marcos@gmail.com>
2022-08-11 16:25:30 -0300

    creating the project

---
+++ src/main/java/com/evereats/fooder/FooderApiApplication.java
@@ -0,0 +1,13 @@
+package com.evereats.fooder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evereats/fooder/FooderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evereats.fooder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
