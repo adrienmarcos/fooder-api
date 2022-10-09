352ead8c945cdf822a11d4f7ac3886b32a6e6d56
Adrien Rossato <adrien.marcos@gmail.com>
2022-10-09 20:32:49 -0300

    implementing a dynamic query

--- src/main/java/com/evereats/fooder/domain/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/evereats/fooder/domain/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.evereats.fooder.domain.infrastructure.repository;
 import com.evereats.fooder.domain.model.Restaurant;
 import com.evereats.fooder.domain.repository.RestaurantRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -17,13 +20,27 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
 
     @Override
     public List<Restaurant> find(String name, BigDecimal initialFreightTax, BigDecimal finalFreightTax) {
-        var jpql = "SELECT r FROM Restaurant r " +
-                   "WHERE r.name LIKE :name AND r.freightTax BETWEEN :initialFreightTax AND :finalFreightTax";
+        var parameters = new HashMap<String, Object>();
+        var jpql = new StringBuilder();
+        jpql.append("SELECT r FROM Restaurant r WHERE 0 = 0 ");
 
-        return entityManager.createQuery(jpql, Restaurant.class)
-                .setParameter("name", "%"+name+"%")
-                .setParameter("initialFreightTax", initialFreightTax)
-                .setParameter("finalFreightTax", finalFreightTax)
-                .getResultList();
+        if (StringUtils.hasLength(name)) {
+            jpql.append("AND name LIKE :name ");
+            parameters.put("name", "%"+name+"%");
+        }
+
+        if (initialFreightTax != null) {
+            jpql.append("AND freightTax >= :initialFreightTax ");
+            parameters.put("initialFreightTax", initialFreightTax);
+        }
+        if (finalFreightTax != null) {
+            jpql.append("AND freightTax <= :finalFreightTax ");
+            parameters.put("finalFreightTax", finalFreightTax);
+        }
+
+        TypedQuery<Restaurant> query = entityManager.createQuery(jpql.toString(), Restaurant.class);
+        parameters.forEach((key, value) -> query.setParameter(key, value));
+
+        return query.getResultList();
     }
 }
