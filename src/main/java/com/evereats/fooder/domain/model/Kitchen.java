aa36f074edaa1554a123599e8fcf243d73e4a408
Adrien Rossato <adrien.marcos@gmail.com>
2022-10-15 18:01:13 -0300

    many to many

--- src/main/java/com/evereats/fooder/domain/model/Kitchen.java
+++ src/main/java/com/evereats/fooder/domain/model/Kitchen.java
@@ -1,10 +1,13 @@
 package com.evereats.fooder.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -20,4 +23,8 @@ public class Kitchen {
 
     @Column(nullable = false)
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "kitchen")
+    private List<Restaurant> restaurants = new ArrayList<>();
 }
