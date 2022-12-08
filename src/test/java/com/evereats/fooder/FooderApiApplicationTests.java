e7922755363b7fba43dcf543657126e286d19f51
Adrien Rossato <adrien.marcos@gmail.com>
2022-12-07 23:18:09 -0300

    creating our first tests

--- src/test/java/com/evereats/fooder/FooderApiApplicationTests.java
+++ src/test/java/com/evereats/fooder/FooderApiApplicationTests.java
@@ -1,13 +1,43 @@
 package com.evereats.fooder;
 
+import com.evereats.fooder.domain.model.Kitchen;
+import com.evereats.fooder.domain.service.KitchenService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import javax.validation.ConstraintViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 @SpringBootTest
+@DisplayName("Tests for kitchen register service")
 class FooderApiApplicationTests {
 
+	@Autowired
+	private KitchenService kitchenService;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("save kitchen")
+	void save_PersistKitchen_whenSuccessful() {
+		var kitchenToBeSaved = new Kitchen();
+		kitchenToBeSaved.setName("chinese");
+		var kitchenSaved = kitchenService.save(kitchenToBeSaved);
+
+		Assertions.assertThat(kitchenSaved).isNotNull();
+		Assertions.assertThat(kitchenSaved.getId()).isNotNull();
+		Assertions.assertThat(kitchenSaved.getName()).isEqualTo(kitchenToBeSaved.getName());
+	}
+
+	@Test()
+	void save_FailPersistKitchen_whenSucessful() {
+		assertThrows(ConstraintViolationException.class, () -> {
+			var kitchenToBeSaved = new Kitchen();
+			kitchenToBeSaved.setName(null);
+			var kitchenSaved = kitchenService.save(kitchenToBeSaved);
+		});
 	}
 
 }
