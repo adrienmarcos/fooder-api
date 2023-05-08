1756188cf255dd24549f439a59372d8a3d71ce12
Adrien Rossato <adrien.marcos@gmail.com>
2023-05-08 19:30:36 -0300

    test: Should register a Kitchen if valid data is provided

--- src/test/java/com/evereats/fooder/KitchenRegisterIntegrationTests.java
+++ src/test/java/com/evereats/fooder/KitchenRegisterIntegrationTests.java
@@ -7,37 +7,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import javax.validation.ConstraintViolationException;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 @SpringBootTest
-@DisplayName("Tests for kitchen register service")
-class FooderApiApplicationTests {
+class KitchenRegisterIntegrationTests {
 
 	@Autowired
 	private KitchenService kitchenService;
 
 	@Test
-	@DisplayName("save kitchen")
-	void save_PersistKitchen_whenSuccessful() {
+	@DisplayName("Should register a Kitchen if valid data is provided")
+	public void save_PersistKitchen_whenSuccessful() {
 		var kitchenToBeSaved = new Kitchen();
-		kitchenToBeSaved.setName("chinese");
-		var kitchenSaved = kitchenService.save(kitchenToBeSaved);
+		kitchenToBeSaved.setName("Chinese");
+		var savedKitchen = kitchenService.save(kitchenToBeSaved);
 
-		Assertions.assertThat(kitchenSaved).isNotNull();
-		Assertions.assertThat(kitchenSaved.getId()).isNotNull();
-		Assertions.assertThat(kitchenSaved.getName()).isEqualTo(kitchenToBeSaved.getName());
+		Assertions.assertThat(savedKitchen).isNotNull();
+		Assertions.assertThat(savedKitchen.getId()).isNotNull();
+		Assertions.assertThat(savedKitchen.getName()).isEqualTo(kitchenToBeSaved.getName());
 	}
-
-	@Test()
-	void save_FailPersistKitchen_whenSucessful() {
-		assertThrows(ConstraintViolationException.class, () -> {
-			var kitchenToBeSaved = new Kitchen();
-			kitchenToBeSaved.setName(null);
-			var kitchenSaved = kitchenService.save(kitchenToBeSaved);
-		});
-	}
-
 }
