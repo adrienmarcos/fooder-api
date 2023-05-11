de56ff049a326da3f918c39dd945f3c53611b658
Adrien Rossato <adrien.marcos@gmail.com>
2023-05-11 11:05:20 -0300

    test: should return status 201 at url /kitchens using Post method

--- src/test/java/com/evereats/fooder/integration/kitchen/KitchenControllerIT.java
+++ src/test/java/com/evereats/fooder/integration/kitchen/KitchenControllerIT.java
@@ -2,6 +2,7 @@ package com.evereats.fooder.integration.kitchen;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,12 +15,29 @@ public class KitchenControllerIT {
     @LocalServerPort
     private int port;
 
-    @Test
-    @DisplayName("Should test the Api at url /kitchens using GET method")
-    public void get_shouldReturnStatus200_whenSuccessful() {
+    @BeforeEach
+    public void beforeEach() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        RestAssured.given().basePath("/kitchens").port(port).accept(ContentType.JSON)
+        RestAssured.port = port;
+        RestAssured.basePath = "/kitchens";
+    }
+
+    @Test
+    @DisplayName("Should return status 200 at url /kitchens using GET method")
+    public void get_shouldReturnStatus200_whenSuccessful() {
+        RestAssured.given()
+                .accept(ContentType.JSON)
                 .when().get()
                 .then().statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("Should return status 201 at url /kitchens using POST method")
+    public void post_shouldReturnStatus201_whenSuccessful() {
+        RestAssured.given()
+                .body("{ \"name\": \"Turkish\" }").contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when().post()
+                .then().statusCode(HttpStatus.CREATED.value());
     }
 }
